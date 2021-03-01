47ac1b31cf2526f1ec991d411aaf12ab5ff27069
Th3At0nic <islammdrahatul@gmail.com>
2021-03-01 23:50:45 +0600

    random commit

--- src/components/Separate/Separate.js
+++ src/components/Separate/Separate.js
@@ -4,10 +4,15 @@ const Separate = (props) => {
   console.log(props);
   const cartPlayers = props.players;
   console.log(cartPlayers);
-  let array = [];
+  let array = [
+    // { ...cartPlayers },
+  ];
+  //   console.log(array);
   array = cartPlayers;
-  //   array = { ...array, ...cartPlayers };
+  //   array.push({ ...cartPlayers });
   console.log(array);
+  //   array = { ...array, ...cartPlayers };
+  //   console.log(array);
   //   const { name, country, salary, age, role } = cartPlayers;
   //   for (let i = 0; i < cartPlayers.length; i++) {
   //     var element = cartPlayers[i];
@@ -24,21 +29,23 @@ const Separate = (props) => {
 
       {/* <Player eachCricketer={array.map((cricketer) => cricketer)}></Player> */}
       <Player eachCricketer={array[0]}></Player>
-      <Player eachCricketer={array[1]}></Player>
-      <Player eachCricketer={array[2]}></Player>
+      {/* <Player eachCricketer={array[1]}></Player>
+      <Player eachCricketer={array[2]}></Player> */}
     </div>
   );
 };
 
 function Player(properties) {
-  console.log(properties);
+  console.log(properties.name);
+  const rahat = properties;
+  console.log(rahat);
   const { name, age } = properties;
   //   console.log(name);
   //   const [] = properties;
   //   console.log(name, country);
   return (
     <div>
-      <h2>Name: {name} </h2>
+      <h2>Name: </h2>
       <h2>Age: </h2>
       <br />
       <br />
