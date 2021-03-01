f36f5521df69fbd38bf67fd00f09413ef31709e4
Th3At0nic <islammdrahatul@gmail.com>
2021-03-01 13:43:06 +0600

    random commit

--- src/components/Players/Person/Person.js
+++ src/components/Players/Person/Person.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "./Person.css";
 
 const Person = (props) => {
   console.log(props.player);
-  const { name, country } = props.player;
+  const { name, country, img, salary, age, role } = props.player;
   return (
-    <div>
-      <h3>This is {name}</h3>
+    <div className="person">
+      <div className="person-img">
+        <img src={img} alt="" />
+      </div>
+      <div className="person-detail">
+        <h2>Name: {name}</h2>
+        <div className="detail-style">
+          <h3>Country: {country}</h3>
+          <h3>Age: {age}</h3>
+          <h3>Role: {role}</h3>
+          <h2>Cost: {salary}</h2>
+        </div>
+      </div>
     </div>
   );
 };
